2edbf82c6f53e2d0fab2de1842201eb44cdaab6b
Obuji Chukwuemeka <obujichukwuemeka@gmail.com>
2022-09-23 00:06:18 +0000

    fix

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * string_toupper - capitalize a string
+ *
+ * @str: pointer
+ * Return: capitalzied string
+ */
+
+char *string_toupper(char *str)
+
+{
+	int i;
+
+	for (i = 0; str[i] != '\0'; i++)
+
+	{
+		if (str[i] >= 'a' && str[i] <= 'z')
+
+		{
+			str[i] = str[i] - 32;
+		}
+	}
+
+	return (str);
+
+}
